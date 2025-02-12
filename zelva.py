4d4380026aebe69641300cecc51d9eb6fe84b8fa
Toumesis <148939971+Toumesis@users.noreply.github.com>
2025-02-12 23:08:33 +0100

    Create zelva.py

---
+++ zelva.py
@@ -0,0 +1,24 @@
+from turtle import forward, left, right, shape, exitonclick
+import math
+
+uhloprickaCtverce = int(math.sqrt(5000))
+strecha = int(math.sqrt(1250))
+shape('turtle')
+left(90)
+forward(50)
+right(90)
+forward(50)
+right(135)
+forward(uhloprickaCtverce)
+left(135)
+forward(50)
+left(90)
+forward(50)
+left(45)
+forward(strecha)
+left(90)
+forward(strecha)
+left(90)
+forward(uhloprickaCtverce)
+
+exitonclick()
